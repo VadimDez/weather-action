47e1fd4708ee7401f9d14696e328fdae3d3126cb
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2020-02-01 12:35:27 +0100

    update code

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-// const request = require("request-promise");
+const request = require("request-promise");
 
 (async () => {
   try {
     const lat = core.getInput("lat");
     const long = core.getInput("long");
     console.log(`lat: ${lat}, long: ${long}`);
-    // const uri = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
+    const uri = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
 
-    // const response = await request({
-    //   uri: uri,
-    //   json: true
-    // });
-    // console.log(JSON.stringify(response));
+    const response = await request({
+      uri: uri,
+      json: true
+    });
+    console.log(JSON.stringify(response));
 
     // core.setOutput("weather", response);
     core.setOutput("weather", `lat: ${lat}, long: ${long}`);
