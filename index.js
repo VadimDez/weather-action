650f08622aca54768944fc13591031938776a094
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2020-02-01 12:29:52 +0100

    update code

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const request = require("request-promise");
+// const request = require("request-promise");
 
 (async () => {
   try {
     const lat = core.getInput("lat");
     const long = core.getInput("long");
     console.log(`lat: ${lat}, long: ${long}`);
-    const uri = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
+    // const uri = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
 
-    const response = await request({
-      uri: uri,
-      json: true
-    });
-    console.log(JSON.stringify(response));
+    // const response = await request({
+    //   uri: uri,
+    //   json: true
+    // });
+    // console.log(JSON.stringify(response));
 
-    core.setOutput("weather", response);
+    // core.setOutput("weather", response);
+    core.setOutput("weather", `lat: ${lat}, long: ${long}`);
     // Get the JSON webhook payload for the event that triggered the workflow
     // const payload = JSON.stringify(github.context.payload, undefined, 2);
     // console.log(`The event payload: ${payload}`);
