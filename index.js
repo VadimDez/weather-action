d0fbda34de1edf08a08c82191e82c80969cc3c8a
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2020-02-01 12:07:51 +0100

    update code

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ const request = require("request-promise");
 
     core.setOutput("weather", response);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2);
+    // console.log(`The event payload: ${payload}`);
   } catch (error) {
+    console.log(error);
     core.setFailed(error.message);
   }
 })();
